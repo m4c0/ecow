61718f20c9ae2c2a0805805d0aec3e5379772c90
Eduardo Costa <m4c0@users.noreply.github.com>
2022-12-19 13:00:19 +0000

    small tweak in "b" poc

--- poc/b/build.cpp
+++ poc/b/build.cpp
@@ -6,9 +6,9 @@ using namespace ecow;
 int main(int argc, char **argv) {
   auto all = unit::create<seq>("all");
 
-  auto myapp = all->add_unit<exe>("my_cli");
+  auto myapp = all->add_unit<app>("bee");
   myapp->add_wsdep("a", exported());
-  myapp->add_unit<>("user");
+  myapp->add_unit<>("bee-exe");
 
   return run_main(all, argc, argv);
 }
