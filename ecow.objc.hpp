ffe9b07a697e026279f715bb8d103ac12712ae8d
Eduardo Costa <m4c0@users.noreply.github.com>
2023-01-30 11:38:27 +0000

    re-adding clang modules for objc

--- ecow.objc.hpp
+++ ecow.objc.hpp
@@ -6,8 +6,18 @@ namespace ecow {
 class objc : public unit {
 protected:
   void build_self() const override {
-    if (target_supports(objective_c))
-      unit::build_self();
+    if (!target_supports(objective_c))
+      return;
+
+    const auto mod_cache_path =
+        impl::current_target()->module_cache_path().string();
+
+    impl::clang{name(), obj_name(name())}
+        .add_arg("-c")
+        .add_arg("-fmodules")
+        .add_arg("-fmodules-cache-path=" + mod_cache_path)
+        .with_deps()
+        .run();
   }
   [[nodiscard]] virtual pathset self_objects() const override {
     return target_supports(objective_c) ? unit::self_objects() : pathset{};
