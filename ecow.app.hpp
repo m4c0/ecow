1f2ffdc9c02afabe41e91715b467391dddb8889a
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-19 09:51:02 +0100

    bundling code after resource copy

--- ecow.app.hpp
+++ ecow.app.hpp
@@ -14,12 +14,13 @@ protected:
 
   void build_self() const override {
     exe::build_self();
-    impl::current_target()->bundle(name(), *this);
 
     const auto res_fld = impl::current_target()->resource_path(name());
     for (const auto &res : resources()) {
       impl::run_copy(res, res_fld / res.filename());
     }
+
+    impl::current_target()->bundle(name(), *this);
   };
 
 public:
