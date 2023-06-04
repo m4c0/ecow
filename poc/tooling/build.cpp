31d2a61ba4671e971adccd055cdc4ab1668dfba3
Eduardo Costa <m4c0@users.noreply.github.com>
2023-06-04 14:19:18 +0100

    fix tool name in poc

--- poc/tooling/build.cpp
+++ poc/tooling/build.cpp
@@ -5,7 +5,7 @@ using namespace ecow;
 int main(int argc, char **argv) {
   auto all = unit::create<seq>("all");
 
-  auto t = all->add_unit<tool>("tool.exe");
+  auto t = all->add_unit<tool>("tool");
   t->add_unit<>("tool");
 
   all->add_unit<sys>(t->executable().string());
