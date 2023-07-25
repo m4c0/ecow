fd5864ddff4e84e85cafe9867a22c6db6805bdc7
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-25 16:09:44 +0100

    poc/mod-deps: peeking tokens + sketch of the job

--- poc/mod-deps/build.cpp
+++ poc/mod-deps/build.cpp
@@ -2,15 +2,7 @@
 
 class tokeniser {
   std::ifstream m_in;
-
-public:
-  tokeniser() {
-    const auto dep_file =
-        ecow::impl::current_target()->build_folder() + "compile_deps.json";
-    m_in = std::ifstream{dep_file};
-    if (!m_in)
-      throw std::runtime_error("Failure opening compile deps");
-  }
+  std::string m_peeked;
 
   std::string next_string() {
     std::string res{};
@@ -53,30 +45,57 @@ public:
     }
   }
 
-  std::string next_token() {
-    switch (char c = m_in.get()) {
-    case ' ':
-    case '\n':
-    case '\r':
-    case '\t':
-      return next_token();
-    case '{':
-    case '}':
-    case '[':
-    case ']':
-    case ':':
-    case ',':
-      return std::string{c};
-    case 't':
-    case 'f':
-      return next_literal(c);
-    case '"':
-      return next_string();
-    default:
-      if (c >= '0' && c <= '9')
-        return next_number(c);
-      return "";
+  std::string read_token() {
+    while (true) {
+      switch (char c = m_in.get()) {
+      case ' ':
+      case '\n':
+      case '\r':
+      case '\t':
+        continue;
+      case '{':
+      case '}':
+      case '[':
+      case ']':
+      case ':':
+      case ',':
+        return std::string{c};
+      case 't':
+      case 'f':
+        return next_literal(c);
+      case '"':
+        return next_string();
+      default:
+        if (c >= '0' && c <= '9')
+          return next_number(c);
+        return "";
+      }
     }
+  }
+
+public:
+  tokeniser() {
+    const auto dep_file =
+        ecow::impl::current_target()->build_folder() + "compile_deps.json";
+    m_in = std::ifstream{dep_file};
+    if (!m_in)
+      throw std::runtime_error("Failure opening compile deps");
+  }
+
+  std::string next() {
+    if (m_peeked != "") {
+      auto res = m_peeked;
+      m_peeked = "";
+      return res;
+    }
+    return read_token();
+  }
+
+  std::string peek() {
+    if (m_peeked == "") {
+      m_peeked = read_token();
+    }
+    return m_peeked;
   }
 };
 
@@ -87,12 +106,19 @@ public:
   void build_self() const override {
     tokeniser t{};
 
+    // find_object();
+    // find_array_attr("rules", []{
+    //   find_object()
+    //   auto pcm = find_string_attr("primary-output")
+    //   find_object_attr("requires", []{
+    //     auto dep = find_string_attr("source-path")
+
     std::string token;
-    while ((token = t.next_token()) != "") {
+    while ((token = t.next()) != "") {
       std::cerr << "T: " << token << std::endl;
     }
 
-    throw 1;
+    throw "OK";
   }
   void create_self_cdb(std::ostream &o) const override {}
 };
