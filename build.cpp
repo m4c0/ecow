400104d73208b1be4bd7be746b00fc677bf10446
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-27 18:53:38 +0100

    build: preparing to switch compilers for meta/non-meta

--- build.cpp
+++ build.cpp
@@ -1,13 +1,33 @@
+#define ECOW_META_BUILD
 #include "ecow.hpp"
 
-int main(int argc, char **argv) {
-  using namespace ecow;
+using namespace ecow;
 
-  auto bld = unit::create<tool>("build");
-  bld->add_unit("build")->add_include_dir("../ecow");
+class builder : public exe {
+protected:
+  virtual void build_self() const override {
+    if (impl::current_target()->supports(host))
+      exe::build_self();
+  }
+
+public:
+  using exe::exe;
+
+  [[nodiscard]] auto executable() const {
+    return impl::host_target{}.exe_name(name());
+  }
+};
+
+int main(int argc, char **argv) {
+  auto bld = unit::create<builder>("build");
+  auto u = bld->add_unit("build");
+
+  if (!std::filesystem::exists("ecow.hpp")) {
+    u->add_include_dir("../ecow");
+  }
 
   auto all = unit::create<seq>("all");
   all->add_ref(bld);
-  all->add_unit<sys>(bld->executable().string());
+  all->add_unit<sys>(bld->executable());
   return run_main(all, argc, argv);
 }
